6e938a2eca357d533a577e4f65af659a2e02a60d
unknown <albarkhan60@gmail.com>
2024-01-31 15:30:21 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./index.css";
 const btn = [
   "C",
-  "%",
+  "=",
   "/",
   "X",
   "-",
@@ -26,20 +26,30 @@ export default function App() {
 }
 
 function Calculator() {
+  const [calinput, setInput] = useState("0");
+  const [result, setResult] = useState("");
+
+  function handleClick(value) {
+    if (value === "=") {
+      // const evaluateExpression = eval(ClipboardEvent).to;
+      setResult(eval(calinput).toString());
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  }
+
   return (
     <div className="calculator">
       <div className="display">
-        <p className="result">9</p>
-        <input
-          className="input-value"
-          value={"0 + 10 + 9010 + 10 + 9010 + 10 + 9010 + 10 + 9010 + 10 + 90"}
-          readOnly
-        ></input>
+        <p className="result">{result}</p>
+        <input className="input-value" value={calinput} readOnly></input>
         {/* <p className="input-value">10+10+9010+10+9010+10+9010+10+9010+10+90</p> */}
       </div>
       <div className="Buttons">
         {btn.map((bt) => (
-          <button>{bt}</button>
+          <button key={bt} onClick={() => handleClick(bt)}>
+            {bt}
+          </button>
         ))}
       </div>
     </div>
