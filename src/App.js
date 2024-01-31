d52af5e1af98cb1133129d6a294a0b49a2fab42e
unknown <albarkhan60@gmail.com>
2024-01-31 11:26:28 +0530

    ui done

--- src/App.js
+++ src/App.js
@@ -29,8 +29,13 @@ function Calculator() {
   return (
     <div className="calculator">
       <div className="display">
-        <p className="input-value">Input Value</p>
-        <p className="reuslt">9</p>
+        <p className="result">9</p>
+        <input
+          className="input-value"
+          value={"0 + 10 + 9010 + 10 + 9010 + 10 + 9010 + 10 + 9010 + 10 + 90"}
+          readOnly
+        ></input>
+        {/* <p className="input-value">10+10+9010+10+9010+10+9010+10+9010+10+90</p> */}
       </div>
       <div className="Buttons">
         {btn.map((bt) => (
